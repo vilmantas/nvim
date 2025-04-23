3b397dcc11e1f957d08511f15411372f5e77e174
Vilmantas Petrusevicius <15175886+vilmantas@users.noreply.github.com>
2025-04-23 16:21:57 +0300

    Updates

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,9 +4,16 @@ return {
     branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function() 
-        require("telescope").setup({})
+        require("telescope").setup({
+            defaults = {
+                preview = {
+                    treesitter = true,
+                }
+            }
+        })
         vim.keymap.set("n", "<leader>ca", function()
             require("telescope.builtin").lsp_code_actions()
         end, { desc = "LSP Code Actions" })
+
     end
 }
