ea131cd30fcd56fe91489b6402216a2b886b137c
Vilmantas Petrusevicius <15175886+vilmantas@users.noreply.github.com>
2025-05-04 09:51:14 +0300

    Use nvim-tree instead of oil

---
+++ after/plugin/nvim-tree.lua
@@ -0,0 +1,24 @@
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+
+require("nvim-tree").setup({
+  view = {
+    width = 30,
+    side = "left",
+    number = false,
+    relativenumber = false,
+  },
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+        folder = true,
+        file = true,
+      },
+    },
+  },
+  filters = {
+    dotfiles = false,
+  },
+})
+
