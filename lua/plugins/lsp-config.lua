dd5a8184bb4878cf3876618c811cab1f92f8883f
Vilmantas <15175886+vilmantas@users.noreply.github.com>
2025-05-04 19:14:16 +0300

    Something

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,18 +18,37 @@ return {
     lazy = false,
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
       local lspconfig = require("lspconfig")
+
       lspconfig.ruby_lsp.setup({
         capabilities = capabilities
       })
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
-        end,
-    },
+
+      local omnisharp_cmd = vim.fn.stdpath("data") .. "\\mason\\bin\\omnisharp.cmd"
+
+      lspconfig.omnisharp.setup({
+        capabilities = capabilities,
+        cmd = {
+          omnisharp_cmd,
+          "-z",
+          "--hostPID",
+          tostring(vim.fn.getpid()),
+          "DotNet:enablePackageRestore=false",
+          "--encoding",
+          "utf-8",
+          "--languageserver",
+          "FormattingOptions:EnableEditorConfigSupport=true",
+          "Sdk:IncludePrereleases=true",
+        },
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+    end,
+  },
 }
