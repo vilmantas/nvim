6981414fe3bd78a2f894723c8ef84cfdeda495dd
Vilmantas Petrusevicius <15175886+vilmantas@users.noreply.github.com>
2025-05-06 09:35:12 +0300

    Add highlighted search

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,17 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope git files' }
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = "Telescope live grep" }) 
 vim.keymap.set('n', '<leader><leader>', builtin.oldfiles, { desc = "Telescope old files" }) 
 
+
+local function grep_visual_selection()
+  local text = vim.fn.getreg("v")
+  builtin.live_grep({ default_text = text })
+end
+
+
+vim.keymap.set("v", "<leader>ps", function()
+  -- Yank visual selection to register v
+  vim.cmd('normal! "vy')
+  grep_visual_selection()
+end, { noremap = true, silent = true })
+
+
