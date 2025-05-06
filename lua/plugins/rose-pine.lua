ec9b8c14c3fbe1e58e02670355f69ff28b34583d
Vilmantas Petrusevicius <15175886+vilmantas@users.noreply.github.com>
2025-05-06 15:19:56 +0300

    Update theme

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -1,22 +1,21 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	lazy = false,
-	priority = 1000,
-	config = function()
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = false,
+    priority = 1000,
+    config = function()
         require("rose-pine").setup({
             variant = "auto",
             dark_variant = "moon",
             disable_italics = true,
             italics = false,
             highlight_groups = {
-            ColorColumn = { bg = "rose" },
-            CursorLine = { bg = "rose" },
-            CursorLineNr = { fg = "love" },
-            LineNr = { fg = "love" },
-            StatusLineNC = { fg = "muted" },
-            StatusLine = { fg = "muted", bg = "love" },
-            NormalFloat = { bg = "base" },
+                CursorLine = { bg = "rose" },
+                CursorLineNr = { fg = "love" },
+                LineNr = { fg = "love" },
+                StatusLineNC = { fg = "muted" },
+                StatusLine = { fg = "muted" },
+                NormalFloat = { bg = "base" },
             },
         })
 
