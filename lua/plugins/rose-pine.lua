ef58a1c57429731212c6b5ffbfee33e0f46491ca
Vilmantas Petrusevicius <15175886+vilmantas@users.noreply.github.com>
2025-05-06 15:17:31 +0300

    Update theme

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -4,7 +4,22 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme "rose-pine"
-	end
+        require("rose-pine").setup({
+            variant = "auto",
+            dark_variant = "moon",
+            disable_italics = true,
+            italics = false,
+            highlight_groups = {
+            ColorColumn = { bg = "rose" },
+            CursorLine = { bg = "rose" },
+            CursorLineNr = { fg = "love" },
+            LineNr = { fg = "love" },
+            StatusLineNC = { fg = "muted" },
+            StatusLine = { fg = "muted", bg = "love" },
+            NormalFloat = { bg = "base" },
+            },
+        })
 
+		vim.cmd.colorscheme "rose-pine"
+	end,
 }
