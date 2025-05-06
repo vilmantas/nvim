f7a9498791d8693ba7ac3843dce5ef108bf44f39
Vilmantas Petrusevicius <15175886+vilmantas@users.noreply.github.com>
2025-05-06 15:05:51 +0300

    Fix ruby LSP. OldFiles work on project level

--- after/plugin/lsp-config.lua
+++ after/plugin/lsp-config.lua
@@ -1,12 +1,15 @@
-vim.api.nvim_create_autocmd("BufReadPost", {
-  callback = function(args)
-    local lspconfig_util = require("lspconfig.util")
-    local fname = vim.api.nvim_buf_get_name(args.buf)
-    local root = lspconfig_util.root_pattern("Gemfile", ".git")(fname)
+local util = require("lspconfig.util")
 
-    if root and root ~= vim.fn.getcwd() then
-      vim.cmd("lcd " .. root)
-    end
+require("lspconfig").ruby_lsp.setup({
+  root_dir = function(fname)
+    -- Search upwards for a directory with both Gemfile and Gemfile.lock
+    return util.search_ancestors(fname, function(path)
+      local gemfile = util.path.join(path, "Gemfile")
+      local lockfile = util.path.join(path, "Gemfile.lock")
+      if util.path.is_file(gemfile) and util.path.is_file(lockfile) then
+        return path
+      end
+    end)
   end,
 })
 
---
+++ lua/plugins/project.lua
@@ -0,0 +1,10 @@
+return {
+  "ahmedkhalf/project.nvim",
+  config = function()
+    require("project_nvim").setup({
+            manual_mode = true
+        })
+    require("telescope").load_extension("projects")
+  end
+}
+
