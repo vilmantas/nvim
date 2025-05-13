8c4ae2eaebf30fe6ce6adb4a900fa17cc466f670
Vilmantas Petrusevicius <15175886+vilmantas@users.noreply.github.com>
2025-05-13 10:21:09 +0300

    Add file diagnostics keybind

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,8 +2,10 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Telescope git files' })
-vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = "Telescope live grep" }) 
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fd", require("telescope.builtin").diagnostics, { desc = "Telescope diagnostics" })
+
 
 vim.keymap.set('n', '<leader><leader>', function()
   require("telescope.builtin").oldfiles({ only_cwd = true, cwd = vim.fn.getcwd() })
